bc18dc9364acd88e2f589131860ed88178a1041b
sameerck18 <120164217+sameerck18@users.noreply.github.com>
2022-12-19 12:52:09 +0530

    Create main.py

---
+++ src/model/main.py
@@ -0,0 +1,78 @@
+# Import libraries
+import argparse
+import glob
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, classification_report
+import mlflow
+
+def main(args):
+    # enable autologging
+    mlflow.autolog()
+
+
+    # read data
+    df = get_csvs_df(args.training_data)
+
+    # split data
+    X_train, X_test, y_train, y_test = split_data(df)
+
+    # train model
+    train_model(args.n_estimators, X_train, X_test, y_train, y_test)
+
+#function to read CSV file
+def get_csvs_df(path):
+    if not os.path.exists(path):
+        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
+    csv_files = glob.glob(f"{path}/*.csv")
+    if not csv_files:
+        raise RuntimeError(f"No CSV files found in provided data path: {path}")
+    return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
+
+# function to split data
+def split_data(df):
+    X = df[['fixed acidity','volatile acidity','citric acid','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol']].values
+    y = df['quality']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test
+
+# function to train model
+def train_model(n_estimators, X_train, X_test, y_train, y_test):
+    RandomForestClassifier(n_estimators=n_estimators).fit(X_train, y_train)
+
+# function to read input arguments
+def parse_args():
+    # setup arg parser
+    parser = argparse.ArgumentParser()
+
+    # add arguments
+    parser.add_argument("--training_data", dest='training_data',
+                        type=str)
+    parser.add_argument("--n_estimators", dest='n_estimators',
+                        type=float, default=200)
+
+    # parse args
+    args = parser.parse_args()
+
+    # return args
+    return args
+
+# run script
+if __name__ == "__main__":
+    # add space in logs
+    print("\n\n")
+    print("*" * 60)
+
+    # parse args
+    args = parse_args()
+
+    # run main function
+    main(args)
+
+    # add space in logs
+    print("*" * 60)
+    print("\n\n")
+
+
