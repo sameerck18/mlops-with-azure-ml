1534f1be0a38c2626c37818aecb12665300d13a3
sameerck18 <120164217+sameerck18@users.noreply.github.com>
2022-12-26 21:18:08 +0530

    Update main.py

--- src/model/main.py
+++ src/model/main.py
@@ -8,10 +8,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, classification_report
 import mlflow
 
+
 def main(args):
     # enable autologging
     mlflow.autolog()
-
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -22,7 +22,7 @@ def main(args):
     # train model
     train_model(args.n_estimators, X_train, X_test, y_train, y_test)
 
-#function to read CSV file
+# function to read CSV file
 def get_csvs_df(path):
     if not os.path.exists(path):
         raise RuntimeError(f"Cannot use non-existent path provided: {path}")
@@ -31,16 +31,21 @@ def get_csvs_df(path):
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
+
 # function to split data
 def split_data(df):
-    X = df[['fixed acidity','volatile acidity','citric acid','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol']].values
+    X = df[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 
+            'chlorides', 'free sulfur dioxide', 'total sulfur dioxide',
+            'density', 'pH', 'sulphates', 'alcohol']].values
     y = df['quality']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
     return X_train, X_test, y_train, y_test
 
+
 # function to train model
 def train_model(n_estimators, X_train, X_test, y_train, y_test):
     RandomForestClassifier(n_estimators=n_estimators).fit(X_train, y_train)
+
 
 # function to read input arguments
 def parse_args():
@@ -59,8 +64,10 @@ def parse_args():
     # return args
     return args
 
+
 # run script
 if __name__ == "__main__":
+    
     # add space in logs
     print("\n\n")
     print("*" * 60)
@@ -74,5 +81,3 @@ if __name__ == "__main__":
     # add space in logs
     print("*" * 60)
     print("\n\n")
-
-
