3932b46f47a89ac8c57a671cf97ee10d595f7b6c
sameerck18 <120164217+sameerck18@users.noreply.github.com>
2022-12-26 21:27:02 +0530

    Update main.py

--- src/model/main.py
+++ src/model/main.py
@@ -39,8 +39,8 @@ def split_data(df):
             'free sulfur dioxide', 'total sulfur dioxide',
             'density', 'pH', 'sulphates', 'alcohol']].values
     y = df['quality']
-    X_train, X_test, y_train, y_test = 
-    train_test_split(X, y, test_size=0.30, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.30, random_state=0)
     return X_train, X_test, y_train, y_test
 
 
